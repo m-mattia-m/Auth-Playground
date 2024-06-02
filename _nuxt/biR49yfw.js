7d6da976799a1f1924f46ad753a9052bd6485ba8
m-mattia-m <m-mattia-m@users.noreply.github.com>
2024-06-02 15:43:01 +0000

    deploy: b1f5c2497d4a7805a7b17531ea674141201c90c7

--- _nuxt/biR49yfw.js
+++
@@ -1,1 +0,0 @@
-import{_ as o}from"./BD43ozcE.js";import{_ as n,c,d as e,b as a,w as s,o as r}from"./Df9M1nO0.js";const _={};function d(i,u){const t=o;return r(),c("p",null,[e("Coming soon! Try "),a(t,{to:"/oauth2",class:"underline"},{default:s(()=>[e("OAuth 2.0")]),_:1}),e(" instead.")])}const f=n(_,[["render",d]]);export{f as default};
